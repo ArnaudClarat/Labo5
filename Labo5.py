9e17cb7071023a6dded950c1b2f924c0b94d5ee1
Toxiko8 <arnaud.clarat@live.fr>
2019-04-20 16:12:07 +0200

    Problème résolu?

--- Labo5.py
+++ Labo5.py
@@ -18,8 +18,8 @@ import csv
 
 
 def updateStock(choix):
-    with open('stock.csv', 'w', newline='') as csvfile:
-        a = 0
+    #with open('stock.csv', 'w', newline='') as csvfile:
+    pass
 
 
 def payement(stock, choix):
@@ -29,23 +29,22 @@ def payement(stock, choix):
     for i in stock:
         if i[0].lower() == choix:
             prix = float(i[1])
-            pass
-    if prix > 0 :
+    if prix > 0:
         print("Veuillez insérer", str(prix) + "€")
-    if prix <= 0 :
+    if prix <= 0:
         print("Erreur interne, retour au menu")
         affichage(stock)
     while somme < prix:
         j += 1
-        if j == 1 :
-             piece = input(str(j) + "ère pièce : ")
-        else :
+        if j == 1:
+            piece = input(str(j) + "ère pièce : ")
+        else:
             piece = input(str(j) + "ème pièce : ")
         somme += float(piece)
     if somme > prix:
         aRendre = somme - prix
         print(str(aRendre) + "€ rendu")
-    else :
+    else:
         print("Reste", prix - somme, "euros à insérer")
     updateStock(choix)
 
@@ -66,13 +65,14 @@ def main():
     while choix.lower() != "inv":
         stock = []
         with open("stock.csv", 'r', newline='') as csvfile:
-            file = csv.reader(csvfile, delimiter=',', quotechar='|')
+            file = csv.reader(csvfile)
             for row in file:
                 stock.append(row)
         print("\nBienvenue dans ce distributeur de boisson!")
         choix = affichage(stock)
         payement(stock, choix)
     # TODO fonction inventaire
+
 
 if __name__ == "__main__":
     main()
